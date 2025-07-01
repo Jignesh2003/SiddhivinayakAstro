9eb59f4fc03db293ad9933f184b43509b222f2d6
Aditya <adityasinghshekhawat25@gmail.com>
2025-07-01 13:09:02 +0530

    error fix be

--- backend/routes/astrologyRoutes.js
+++ backend/routes/astrologyRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { detailedKundli } from '../controllers/kudliController.js';
+import { detailedKundli, detailedKundliMatching } from '../controllers/kudliController.js';
 
 const router = express.Router()
 
