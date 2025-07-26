d11fc13de791f2df5b137ae8a3e609e7ad5aaa07
Aditya <adityasinghshekhawat25@gmail.com>
2025-07-26 20:51:07 +0530

    chat session not crreated

--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -2,20 +2,21 @@ import express from "express";
 const router = express.Router();
 
 import { createChatRequest, fetchChat, getPendingRequests, getSessionStatus, respondToRequest, updateSessionStatus } from "../controllers/chatController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 router.get("/:sessionId", fetchChat);
 
-router.post("/request",createChatRequest)
+router.post("/request",authMiddleware, createChatRequest)
 
-router.get("/requests/:astrologerId", getPendingRequests);
+router.get("/requests/:astrologerId",authMiddleware, getPendingRequests);
 
-router.patch("/:sessionId/respond", respondToRequest);
+router.patch("/:sessionId/respond",authMiddleware, respondToRequest);
 
 
 //user
-router.get("/status/:sessionId", getSessionStatus);
+router.get("/status/:sessionId",authMiddleware, getSessionStatus);
 
 // update status
-router.patch("/status/:sessionId", updateSessionStatus);
+router.patch("/status/:sessionId",authMiddleware, updateSessionStatus);
 
 export default router;
