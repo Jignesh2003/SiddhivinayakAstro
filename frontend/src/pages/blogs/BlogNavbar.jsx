e26d907dccea4fd6b52f82d526ab059e3d9b3dab
Aadi <adityasinghshekhawat25@gmail.com>
2025-08-23 11:56:50 +0530

    sort logic change

--- frontend/src/pages/blogs/BlogNavbar.jsx
+++ frontend/src/pages/blogs/BlogNavbar.jsx
@@ -9,7 +9,6 @@ const BlogNavbar = () => {
           <Link to="/blogs-astrology-and-you" className="text-white hover:text-yellow-500 text-lg">Astrology & You</Link>
           <Link to="/blogs-power-of-meditation" className="text-white hover:text-yellow-500 text-lg">Power of Meditation</Link>
           <Link to="/blogs-sacred-yantras" className="text-white hover:text-yellow-500 text-lg">Sacred Yantras</Link>
-          <Link to="/blogs-zodiac-signs" className="text-white hover:text-yellow-500 text-lg">Zodiac Signs</Link>
           <Link to="/blogs-planetary-influence" className="text-white hover:text-yellow-500 text-lg">Planetary Influence</Link>
         </div>
       </div>
