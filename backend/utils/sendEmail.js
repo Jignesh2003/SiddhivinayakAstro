2acdbe711c075a11cfd1a97e86e359516a972ab4
Aadi <adityasinghshekhawat25@gmail.com>
2025-08-24 11:42:14 +0530

    Webhook online order check

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,19 +1,23 @@
+//sending email
+
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com", // Explicitly set the SMTP host
+  host: "smtp.hostinger.com", // Explicitly set the SMTP host
   port: 587, // Use 587 for TLS (secure) or 465 for SSL
   secure: false, // Set to true for port 465, false for 587
   auth: {
-    user: 'jokerdclown01@gmail.com',
-    pass: 'nbho gytl cxhv qkxn',
+    user: process.env.MAIN_EMAIL,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
 const sendEmail = async (email, subject, text) => {
   try {
     await transporter.sendMail({
-      from:process.env.EMAIL_USER, // Sender name
+      from: process.env.ALIAS_EMAIL, // Sender name
       to: email,
       subject,
       text,
@@ -22,7 +26,6 @@ const sendEmail = async (email, subject, text) => {
     console.log("✅ Email sent successfully!");
   } catch (error) {
     console.error("🚨 Email send error:", error.response || error.message);
-
   }
 };
 
